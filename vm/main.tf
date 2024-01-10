068d88f68d310a2a9e775ea4f8bf9b6284608de9
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-01-10 16:42:38 +0530

    RHCOS image

--- vm/main.tf
+++ vm/main.tf
@@ -7,37 +7,43 @@ locals {
   vm_id = random_id.label.hex
 }
 
-data "ignition_user" "vm_user" {
-  name                = "core"
-  password_hash = var.password_hash
-}
+#data "ignition_user" "vm_user" {
+#  name                = "core"
+#  password_hash = var.password_hash
+#}
 
-data "ignition_file" "vm_hostname" {
-  count     = var.rhcos_count
-  overwrite = true
-  mode      = "420" // 0644
-  path      = "/etc/hostname"
-  content {
-    content = <<EOF
-${local.vm_id}-rhcos-${count.index}
-EOF
-  }
-}
+#data "ignition_file" "vm_hostname" {
+#  count     = var.rhcos_count
+#  overwrite = true
+#  mode      = "420" // 0644
+#  path      = "/etc/hostname"
+#  content {
+#    content = <<EOF
+#${local.vm_id}-rhcos-${count.index}
+#EOF
+#  }
+#}
 
-data "ignition_config" "vm_ignition" {
-  count = var.rhcos_count
-  users = [data.ignition_user.vm_user.rendered]
-  files = [data.ignition_file.vm_hostname[count.index].rendered]
-}
+#data "ignition_config" "vm_ignition" {
+#  count = var.rhcos_count
+#  users = [data.ignition_user.vm_user.rendered]
+#  files = [data.ignition_file.vm_hostname[count.index].rendered]
+#}
 
-data "ignition_config" "clusternode_ignition" {
-  count = var.rhcos_count
-  users = [data.ignition_user.vm_user.rendered]
-}
+#data "ignition_config" "clusternode_ignition" {
+#  count = var.rhcos_count
+#  users = [data.ignition_user.vm_user.rendered]
+#}
 
-data "ibm_pi_image" "rhcos_image" {
-  pi_image_name        = var.rhcos_image_name
-  pi_cloud_instance_id = var.service_instance_id
+resource "ibm_pi_image" "boot_image" {
+ # provider                  = ibm.powervs
+  pi_image_name             = "rhcos-${var.cluster_id}"
+  pi_cloud_instance_id      = var.workspace_id
+  pi_image_bucket_name      = var.powervs_image_bucket_name
+  pi_image_bucket_access    = "public"
+  pi_image_bucket_region    = var.cos_bucket_location
+  pi_image_bucket_file_name = var.powervs_image_bucket_file_name
+  pi_image_storage_type     = "tier1"
 }
 
 data "ibm_pi_dhcp" "dhcp_service" {
@@ -48,14 +54,53 @@ data "ibm_pi_key" "key" {
   pi_cloud_instance_id = var.service_instance_id
   pi_key_name          = var.keypair_name
 }
+
+data "ibm_resource_group" "cos_group" {
+  name = var.resource_group
+}
+
+#resource "ibm_resource_instance" "cos_instance" {
+#  name              = "${var.cluster_id}-cos"
+#  resource_group_id = data.ibm_resource_group.cos_group.id
+#  service           = "cloud-object-storage"
+#  plan              = "standard"
+#  location          = var.cos_instance_location
+#  tags              = [var.cluster_id]
+#}
+
+# Create an IBM COS Bucket to store ignition
+#resource "ibm_cos_bucket" "ignition" {
+#  bucket_name          = "${var.cluster_id}-bootstrap-ign"
+#  resource_instance_id = ibm_resource_instance.cos_instance.id
+#  region_location      = var.cos_bucket_location
+#  storage_class        = var.cos_storage_class
+#}
+
+#resource "ibm_resource_key" "cos_service_cred" {
+#  name                 = "${var.cluster_id}-cred"
+#  role                 = "Reader"
+#  resource_instance_id = ibm_resource_instance.cos_instance.id
+#  parameters           = { HMAC = true }
+#}
+
+# Place the bootstrap ignition file in the ignition COS bucket
+#resource "ibm_cos_bucket_object" "ignition" {
+#  bucket_crn      = ibm_cos_bucket.ignition.crn
+#  bucket_location = ibm_cos_bucket.ignition.region_location
+#  content         = var.ignition
+#  key             = "bootstrap.ign"
+#  etag            = md5(var.ignition)
+#}
+
+data "ibm_iam_auth_token" "iam_token" {}
+
 resource "ibm_pi_instance" "bootstrap" {
   count                = 1
   pi_memory            = "2"
   pi_processors        = "0.25"
   pi_instance_name     = "${local.vm_id}-rhcos-bootstrap1"
   pi_proc_type         = "shared"
-  pi_image_id          = data.ibm_pi_image.rhcos_image.id
-  pi_storage_pool      = data.ibm_pi_image.rhcos_image.storage_pool
+  pi_image_id          = ibm_pi_image.boot_image.image_id
   pi_key_pair_name     = data.ibm_pi_key.key.id
   pi_sys_type          = var.system_type
   pi_cloud_instance_id = var.service_instance_id
@@ -63,7 +108,13 @@ resource "ibm_pi_instance" "bootstrap" {
   pi_network {
     network_id = data.ibm_pi_dhcp.dhcp_service.network_id
   }
-  pi_user_data         = base64encode(data.ignition_config.vm_ignition[count.index].rendered)
+#   pi_user_data = base64encode(templatefile("${path.module}/templates/bootstrap.ign", {
+#    PROTOCOL    = var.enable_snat ? "https" : "http"
+#    HOSTNAME    = var.enable_snat ? ibm_cos_bucket.ignition.s3_endpoint_public : var.proxy_server_ip
+#    BUCKET_NAME = ibm_cos_bucket.ignition.bucket_name
+#    OBJECT_NAME = ibm_cos_bucket_object.ignition.key
+#    IAM_TOKEN   = data.ibm_iam_auth_token.iam_token.iam_access_token
+#  }))
 
 }
 
@@ -73,8 +124,7 @@ resource "ibm_pi_instance" "cluster_node" {
   pi_processors        = "0.25"
   pi_instance_name     = "${local.vm_id}-rhcos-master1"
   pi_proc_type         = "shared"
-  pi_image_id          = data.ibm_pi_image.rhcos_image.id
-  pi_storage_pool      = data.ibm_pi_image.rhcos_image.storage_pool
+  pi_image_id          = ibm_pi_image.boot_image.image_id
   pi_key_pair_name     = data.ibm_pi_key.key.id
   pi_sys_type          = var.system_type
   pi_cloud_instance_id = var.service_instance_id
@@ -82,7 +132,7 @@ resource "ibm_pi_instance" "cluster_node" {
   pi_network {
     network_id = data.ibm_pi_dhcp.dhcp_service.network_id
   }
-  pi_user_data         = base64encode(data.ignition_config.clusternode_ignition[count.index].rendered)
+  #pi_user_data         = base64encode(data.ignition_config.clusternode_ignition[count.index].rendered)
 }
 resource "time_sleep" "wait_for_bootstrap_macs" {
   create_duration = "3m"
--- vm/variables.tf
+++ vm/variables.tf
@@ -32,7 +32,57 @@ variable "password_hash" {
   default = ""
 }
 
+variable "cos_bucket_location" {
+  type        = string
+  description = "The region in which to create the Cloud Object Store bucket. Used for the igntion file."
+}
+
+variable "cos_instance_location" {
+  type        = string
+  description = "Specifies whether the Cloud Object Store instance is global or in a specific region. Used for the ignition file."
+}
+
+variable "cos_storage_class" {
+  type        = string
+  description = "The storage class for the Cloud Object Store instance."
+}
+
 variable "cloud_instance_id" {
   type        = string
   description = "The Power VS Service Instance (aka Cloud Instance) ID."
+}
+
+variable "cluster_id" {
+  type        = string
+  description = "The ID created by the installer to uniquely identify the created cluster."
+}
+
+variable "enable_snat" {
+  type        = bool
+  description = "Indicates if the DHCP server will have SNAT enabled."
+  default     = true
+}
+
+#variable "ignition" {
+#  type        = string
+#  description = "The ignition file."
+#}
+
+variable "powervs_image_bucket_name" {
+  type        = string
+  description = "Name of the COS bucket containing the image to be imported."
+}
+
+variable "powervs_image_bucket_file_name" {
+  type        = string
+  description = "File name of the image in the COS bucket."
+}
+
+variable "resource_group" {
+  type        = string
+  description = "The name of the Power VS resource group to which the user belongs."
+}
+
+variable "workspace_id" {
+    type = string
 }
